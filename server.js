9fa52f7ffc7e7c0aa86782c342b6b2482540ea78
Preksha Gandhi <prekshagandhi@gmail.com>
2021-11-12 17:13:01 -0500

    Added mongoosedb connection for heroku

--- server.js
+++ server.js
@@ -15,14 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose
-  .connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
