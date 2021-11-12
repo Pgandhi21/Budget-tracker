ed090af016aa49c296dd1eb2055ff579db766380
Preksha Gandhi <prekshagandhi@gmail.com>
2021-11-12 08:15:47 -0500

    Added manifest.webmanifest file

---
+++ public/service-workers.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    './',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    './index.js',
+    './manifest.webmanifest',
+    './styles.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  ];
+
+  
