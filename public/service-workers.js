7fbeea3283ab5c4e5afcbbf24003dc0174387536
Preksha Gandhi <prekshagandhi@gmail.com>
2021-11-12 09:12:06 -0500

    Service-workers.js added

--- public/service-workers.js
+++ public/service-workers.js
@@ -1,11 +1,84 @@
 const FILES_TO_CACHE = [
-    './',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
-    './index.js',
-    './manifest.webmanifest',
-    './styles.css',
-    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
-  ];
+  "./",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./index.js",
+  "./manifest.webmanifest",
+  "./styles.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
 
-  
+const CACHE_NAME = "budget-tracker-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (res) {
+        if (res) {
+          return res;
+        } else if (evt.request.headers.get("accept").includes("text.html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
